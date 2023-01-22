dda6c29d765e623d66651b9b2563f1acaa288be5
Andre Martins Pereira <andre.martins@engesoftware.com.br>
2023-01-22 09:39:36 -0300

    Migrations e customização do Json

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `addres`, `first_name`, `gender`, `last_name`) VALUES 
+    (1,'3142314553 fsef4sedf4sfs','XIIIIIUY','Male','Caiuuuuuuuuu'),
+    (3,'Espanha','Rivaldo','Male','N/A'),
+    (5,'Brasil','Leando e Leonardo','Male','N/A'),
+    (6,'Nordeste','Willian','Male','Boner NErido'),
+    (8,'Nordeste','AAAAAAAAAA','Male','Bernado'),
+    (9,'3142314553 fsef4sedf4sfs','Nelson','Male','Mandela'),
+    (10,'3142314553 fsef4sedf4sfs','Nelson','Male','Mandela');
